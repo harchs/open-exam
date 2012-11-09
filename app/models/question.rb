05fa0b6696fbe191ce3a5dfa6a2bc2e62ac2591a
Nikhil <jkdman63@gmail.com>
2012-11-09 09:13:55 -0500

    fix Guard file

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,11 @@
 class Question < ActiveRecord::Base
-  attr_accessible :name, :quiz_id, :choices_attributes
+  attr_accessible :name, :quiz_id
 
   belongs_to :quiz
   has_many :choices
   has_many :answers
   # This is because we want to eventually allow for checkbox answers
+
   
-  accepts_nested_attributes_for :choices, allow_destroy: true
+  
 end
