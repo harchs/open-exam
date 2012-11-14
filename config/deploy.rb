f213d37bc3cce3977b866dc23d9128180ec1c339
aviflombaum <avi@flombaum.com>
2012-11-14 18:10:54 -0500

    deploy v1

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+set :application, "openexam-app"
+set :repository,  "git@github.com:flatiron-school/open-exam.git"
+
+set :scm, :subversion
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "openexam.org"                          # Your HTTP server, Apache/etc
+role :app, "openexam.org"                          # This may be the same as your `Web` server
+role :db,  "openexam.org", :primary => true # This is where Rails migrations will run
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
