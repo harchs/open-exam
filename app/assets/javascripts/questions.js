255cc5a243d84e1fe98e362372e85d853947ae29
Stephen Chen <stephenchen13@gmail.com>
2012-11-15 12:44:22 -0500

    removed comments

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
-
-  // $(".actions#question_submission > :submit[name='commit']").click( function (e) {
-
-  //    var selected = 0;
-
-  //    $("input[name='is_correct']:checked").each(function(){
-  //      selected += 1;
-  //    });
-
-  //    if (selected == 1) {
-  //    } else {
-  //      alert ("You must select a choice.");
-  //      return false;
-  //    }
-
-  // });
-
   $('input#submit_question').click(function(e){
     if ($('form#new_question input:radio:checked').length <= 0) {
       if ($('#error_explanation').length > 0)
