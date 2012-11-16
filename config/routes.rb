d79ec7c49cb2e8ddf5d8961e40a0b203489746f7
Akiva Leeder <akiva.leeder@gmail.com>
2012-11-16 10:57:14 -0500

    added about and privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 OpenExam::Application.routes.draw do
 
+  get "about", to: 'info#about'
+  get "privacy", to: 'info#privacy'
+
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
