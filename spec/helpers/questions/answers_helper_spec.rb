de007fc2c43684847fd8a259ca9e793e25b7e632
Stephen Chen <stephenchen13@gmail.com>
2012-11-26 11:47:44 -0500

    test baseline

--- spec/helpers/questions/answers_helper_spec.rb
+++ spec/helpers/questions/answers_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe Question::AnswersHelper do
+describe Questions::AnswersHelper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/requests/questions/question_answers_spec.rb
+++ spec/requests/questions/question_answers_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe "Admins" do
-  # describe "GET /admins" do
+describe "Questions::Answers" do
+  # describe "GET /question_answers" do
   #   it "works! (now write some real specs)" do
   #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-  #     get admins_path
+  #     get question_answers_path
   #     response.status.should be(200)
   #   end
   # end
