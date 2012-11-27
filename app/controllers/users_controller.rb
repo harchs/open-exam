d39b268cc7cd76f0eaae76b05e505561ebccda1a
Rex Feng <rexfeng@gmail.com>
2012-11-27 11:42:26 -0500

    allow signup to create new admin and org

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,8 @@ class UsersController < ApplicationController
   # GET /users/new.json
   def new
     @user = User.new(:organization_id => current_org.id)
+    # @user = User.new
+    # @organization = Organization.new
 
     respond_to do |format|
       format.html # new.html.erb
@@ -41,16 +43,21 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
+    # organization_name = params[:user][:organization]
+    # params[:user].delete("organization")
+
     @user = User.new(params[:user])
+    # @organization = Organization.new(:name => organization_name)
 
     respond_to do |format|
-      if @user.save
+      if @user.save# && @organization.save
         OpenExamMailer.registration_confirmation(@user).deliver
+        # @user.update_attributes(:organization_id => @organization.id, :role => "Admin")
         session[:user_id] = @user.id
         format.html { redirect_to root_url, notice: 'Thanks for joining OpenExam!' }
         format.json { render json: @user, status: :created, location: @user }
       else
-        format.html { render action: "new" }
+        format.html { render signup_path }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,8 @@
 class Organization < ActiveRecord::Base
   attr_accessible :name, :subdomain
 
+  validates :name, :presence => :true
+
   has_many :users
   has_many :quizzes
 end
