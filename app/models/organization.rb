49bb1da2f64360fecb73e7da1ec48556fdd6075b
Rex Feng <rexfeng@gmail.com>
2012-11-27 15:50:40 -0500

    an organization is not able to build multiple users with the same email

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,4 +5,11 @@ class Organization < ActiveRecord::Base
 
   has_many :users
   has_many :quizzes
+
+  accepts_nested_attributes_for :users
+  validate :validate_unique_users
+
+  def validate_unique_users
+    validate_uniqueness_of_in_memory(users, [:email, :organization_id], 'User e-mails must be unique.')
+  end
 end
