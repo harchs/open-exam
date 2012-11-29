83e6428d5bf92fafef8a0fee6985f86a07825e74
Akiva Leeder <akiva.leeder@gmail.com>
2012-11-29 15:25:19 -0500

    subdomain sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,11 @@
 # Be sure to restart your server when you modify this file.
-
-OpenExam::Application.config.session_store :cookie_store, key: '_open_exam_session', :domain => :all
+case Rails.env
+when :development
+  domain = 'lvh.me'
+when :production
+  domain = 'openexam.org'
+end
+OpenExam::Application.config.session_store :cookie_store, key: '_open_exam_session', :domain => ".#{domain}"
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
