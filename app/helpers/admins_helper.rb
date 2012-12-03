68d613871678b78f35ad5743109c33a8bee467b3
Rex Feng <rexfeng@gmail.com>
2012-12-03 12:37:31 -0500

    fix double quotes in charts

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -1,2 +1,11 @@
 module AdminsHelper
+
+  def escape_for_js(txtstring)
+    if txtstring.to_s.include?('"')
+      txtstring.to_s.gsub!(/\"/,'\\"').html_safe
+    else
+      txtstring.html_safe
+    end
+  end
+
 end
