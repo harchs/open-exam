572ba01aefad9f50fba79cdce80778727cceac56
bradwheel <bradley.m.wheel@gmail.com>
2012-12-11 17:34:53 -0500

    setup config/application.rb for rspec

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,17 @@ module OpenExam
   class Application < Rails::Application
     config.action_mailer.delivery_method   = :postmark
     config.action_mailer.postmark_settings = { :api_key => APP_CONFIG['development']['postmark_api_key'] }
+
+    config.generators do |g|
+        g.test_framework :rspec,
+            fixtures: true,
+            view_specs: false,
+            helper_specs: false,
+            routing_specs: false,
+            controller_specs: true,
+            request_specs: true
+        g.fixture_replacement :factory_girl, dir: "spec/factories"
+end
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
